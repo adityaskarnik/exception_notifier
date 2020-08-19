de55b9f4697c44eb4597956379bd23510ca80411
Aditya Karnik <aditya.s.karnik@gmail.com>
2020-08-19 11:42:08 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Exception Notifier
+
+While working on our code in Jupyter Notebook we tend to forget after a while that we working actually running some code while there was an exception that interrupted the long running task. :angry::cry: 
+
+COMEON!!!!!! NOT FARE
+
+We again run it and have to wait and watch for another iteration to check if the fix worked or did it not.
+
+Such a pain. :worried:
+
+Why don't we think about a exception notifier. Wait, What?! Don't we already have a notifier? Ahh yes we do, but not an exception notifier.
+
+Well we can use them in each the cells and all that, but that's a mess in the notebook.
+
+We can have an extension, in the notebook where if there was an exception we will be notified like a normal notifier does.
---
+++ exception_notifier.js
@@ -0,0 +1,67 @@
+define([
+    "jquery",
+    "base/js/namespace",
+    "require",
+  ], function ($, Jupyter, requirejs) {
+    "use strict";
+  
+    var params = {
+      sticky: true,
+      play_sound: true
+    };
+    var audio_file = "./notify.mp3";
+  
+    var current_time = function() {
+      return new Date().getTime() / 1000;
+    };
+  
+    var play_notification_sound = function(opts) {
+      try {
+        var audio = new Audio(requirejs.toUrl(audio_file));
+        audio.play();
+      } catch(e) {
+        console.log('HTML5 Audio not supported in browser.');
+      }
+    };
+  
+    var notify = function () {
+        var opts = {
+          body: "Exception",
+          icon: Jupyter.notebook.base_url + "static/base/images/favicon.ico",
+          requireInteraction: params.sticky
+        };
+        if (params.play_sound) {
+          play_notification_sound(opts);
+        }
+    };
+  
+    function showNotification() {
+      notify();
+      const notification = new Notification("Exception Occurred", {
+        body: "Exception occurred in a cell. Please check"
+     })
+    }
+    function checkPermission() {
+      if (Notification.permission === "granted") {
+        showNotification();
+     } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(permission => {
+          if (permission === "granted") {
+            showNotification();
+          }
+           console.log(permission);
+        });
+     }
+    }
+    var load_ipython_extension = function () {
+      return Jupyter.notebook.config.loaded.then(function() {
+        $.extend(true, params, Jupyter.notebook.config.data.notify);
+        checkPermission();
+      });
+    };
+  
+    return {
+      load_ipython_extension : load_ipython_extension
+    };
+  
+  });
