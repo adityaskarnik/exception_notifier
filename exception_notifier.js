f9e6518133444ccbbf5ee8d84170f8c931c3d5c1
Aditya Karnik <aditya.s.karnik@gmail.com>
2020-08-31 17:41:45 +0530

    trying to figureout events for output area

--- exception_notifier.js
+++ exception_notifier.js
@@ -4,14 +4,107 @@ define([
   'moment',
   'base/js/namespace',
   'base/js/events',
-  'notebook/js/codecell'
+  'notebook/js/codecell',
+  'notebook/js/outputarea',
   ], function (
     requirejs,
     $,
     moment,
     Jupyter,
     events,
+    outputarea,
     codecell) {
+      var all_events = [
+        'app_initialized.DashboardApp',
+        'app_initialized.NotebookApp',
+        'autosave_disabled.Notebook',
+        'autosave_enabled.Notebook',
+        'before_save.Notebook',
+        'changed',
+        'checkpoint_created.Notebook',
+        'checkpoint_delete_failed.Notebook',
+        'checkpoint_deleted.Notebook',
+        'checkpoint_failed.Notebook',
+        'checkpoint_restore_failed.Notebook',
+        'checkpoint_restored.Notebook',
+        'checkpoints_listed.Notebook',
+        'collapse_pager',
+        'command_mode.Cell',
+        'command_mode.Notebook',
+        'config_changed.Editor',
+        'create.Cell',
+        'delete.Cell',
+        'draw_notebook_list.NotebookList',
+        'edit_mode.Cell',
+        'edit_mode.Notebook',
+        'execute.CodeCell',
+        'execution_request.Kernel',
+        'expand_pager',
+        'file_load_failed.Editor',
+        'file_loaded.Editor',
+        'file_renamed.Editor',
+        'file_saved.Editor',
+        'file_saving.Editor',
+        'input_reply.Kernel',
+        'kernel_autorestarting.Kernel',
+        'kernel_busy.Kernel',
+        'kernel_connected.Kernel',
+        'kernel_connection_dead.Kernel',
+        'kernel_connection_failed.Kernel',
+        'kernel_created.Kernel',
+        'kernel_created.Session',
+        'kernel_dead.Kernel',
+        'kernel_dead.Session',
+        'kernel_disconnected.Kernel',
+        'kernel_idle.Kernel',
+        'kernel_interrupting.Kernel',
+        'kernel_killed.Kernel',
+        'kernel_killed.Session',
+        'kernel_ready.Kernel',
+        'kernel_reconnecting.Kernel',
+        'kernel_restarting.Kernel',
+        'kernel_starting.Kernel',
+        'kernelspecs_loaded.KernelSpec',
+        'list_checkpoints_failed.Notebook',
+        'mode_changed.Editor',
+        'no_kernel.Kernel',
+        'notebook_copy_failed',
+        'notebook_deleted.NotebookList',
+        'notebook_load_failed.Notebook',
+        'notebook_loaded.Notebook',
+        'notebook_loading.Notebook',
+        'notebook_read_only.Notebook',
+        'notebook_renamed.Notebook',
+        'notebook_restoring.Notebook',
+        'notebook_save_failed.Notebook',
+        'notebook_saved.Notebook',
+        'open_with_text.Pager',
+        'output_appended.OutputArea',
+        'preset_activated.CellToolbar',
+        'preset_added.CellToolbar',
+        'rebuild.QuickHelp',
+        'received_unsolicited_message.Kernel',
+        'rendered.MarkdownCell',
+        'resize',
+        'resize-header.Page',
+        'save_status_clean.Editor',
+        'save_status_dirty.Editor',
+        'select.Cell',
+        'selected_cell_type_changed.Notebook',
+        'send_input_reply.Kernel',
+        'sessions_loaded.Dashboard',
+        'set_dirty.Notebook',
+        'set_next_input.Notebook',
+        'shell_reply.Kernel',
+        'spec_changed.Kernel',
+        'spec_match_found.Kernel',
+        'spec_not_found.Kernel',
+        'trust_changed.Notebook',
+        'unrecognized_cell.Cell',
+        'unrecognized_output.OutputArea',
+        'unregistered_preset.CellToolbar',
+    ];
+    
     "use strict";
   
     var CodeCell = codecell.CodeCell;
@@ -54,20 +147,21 @@ define([
     }
     function checkPermission() {
       if (Notification.permission === "granted") {
-        // TODO add condition to check if exception has occurred
-        var old_get_callbacks = CodeCell.prototype.get_callbacks;
-        CodeCell.prototype.get_callbacks = function () {
-            var callbacks = old_get_callbacks.apply(this, arguments);
-            console.log("callbacks", callbacks);
-            var cell = this;
-            var prev_reply_callback = callbacks.shell.reply;
-            console.log("prev_reply_callback", prev_reply_callback);
-            callbacks.shell.reply = function (msg) {
-            return prev_reply_callback(msg);
-        };
-        return callbacks;
-          
-        };
+        $('.cell .output_area .output_error').each(function (idx, el) {
+          console.log("HERE");
+          console.log(el);
+      });
+      events.on('unrecognized_output.OutputArea', function (evt, data) {
+        // console.log('[evt]', evt.type, (new Date()).toISOString(), data);
+        console.log("unrecognized_output.OutputArea");
+      });
+      events.on('output_appended.OutputArea', function (evt, data) {
+        // console.log('[evt]', evt.type, (new Date()).toISOString(), data);
+        console.log("output_appended.OutputArea");
+      });
+      events.on(all_events.join(' '), function (evt, data) {
+        // showNotification();
+      });
         showNotification();
      } else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(permission => {
